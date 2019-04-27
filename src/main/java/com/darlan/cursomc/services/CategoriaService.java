d0e1e8f60d2dd397fc63d6b26badc9902c56b33e
Darlan <darlan.smedeiros@gmail.com>
2019-04-27 16:30:39 -0300

    Curso de springBoot model

---
+++ src/main/java/com/darlan/cursomc/services/CategoriaService.java
@@ -0,0 +1,24 @@
+package com.darlan.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.darlan.cursomc.domain.Categoria;
+import com.darlan.cursomc.repositories.CategoriaRepository;
+import com.darlan.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class CategoriaService {
+	@Autowired
+	private CategoriaRepository repo;
+	public Categoria find(Integer id) {
+				
+		Optional<Categoria> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+		"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
+		
+	}
+	
+}
